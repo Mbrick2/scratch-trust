0993b4e727caca7edf88b602e6ac678b161cbeb0
LankyBox01 <79767244+LankyBox01@users.noreply.github.com>
2022-01-01 05:06:15 +0200

    Add compact messages: first step

---
+++ message.js
@@ -0,0 +1,7 @@
+let msg = {
+  "en": {
+    "main": "Welcome to ForumsFeed! ForumsFeed is a simple tool to measure your (or someone else's) level of trustworthyness on the <a href='https://scratch.mit.edu/discuss'>Scratch Discussion Forums</a>.",
+    "input": "Scratch username...",
+    "button": "Measure!"
+   }
+ }
