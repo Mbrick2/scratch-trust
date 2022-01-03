bbc02b6c8baad00a654e801291ee56956147439a
Mbrick2 <90691415+Mbrick2@users.noreply.github.com>
2022-01-03 16:45:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.getElementById(rankbtn).style.display = 'block';
+function copy() {
+  text = `I am a ${rank}`
+  copyText.setSelectionRange(0, 99999); 
+  navigator.clipboard.writeText(copyText.text);
+}
 function getworth(username) {
 
  fetch("https://scratchdb.lefty.one/v3/forum/user/info/" + encodeURIComponent(username))
@@ -15,6 +21,8 @@ function getworth(username) {
  				alert("This user does not exist.") }
 
 function alertRating(data) {
+	
+let rank
 
 if (data.counts == null) {
 
@@ -31,32 +39,40 @@ var b = data.counts.total.count;
   } else if (b >= 15000) {
 
     alert(`Veteran - ${b} posts!`);
+    rank = "Veteran"
 
   } else if (b >= 10000) {
 
     alert(`Experienced! - ${b} posts!`);
+    rank = "Experienced"
 
 	} else if (b >= 5000) {
 
     alert(`Trusty - ${b} posts!`);
+    rank = "Trusty"
 
   } else if (b >= 1000) {
 
     alert(`Helper - ${b} posts!`);
+    rank = "Helper"
 
   } else if (b >= 500) {
 
     alert(`Assistive Trainee - ${b} posts!`);
+    rank = "Assistive Trainee"
 
   } else if (b >= 100) {
 
   	alert(`Trainee - ${b} posts!`);
+	rank = "Trainee"
 
   } else {
 
     alert(`Newcomer - ${b} posts!`);
 
   }
+	
+  document.getElementById(rankbtn).style.display = 'none';
 
  } 
 
